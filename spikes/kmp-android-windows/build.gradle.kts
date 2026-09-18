31a2b2914523092cb87e3a0fb7281502d66a805f
Omar Khair <omar.khair70@gmail.com>
2026-09-18 10:15:09 +0300

    spike15: expose serialization plugin

--- spikes/kmp-android-windows/build.gradle.kts
+++ spikes/kmp-android-windows/build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room3) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
 }
