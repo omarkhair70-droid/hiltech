778753c723e56274118ec72f679363d7f791ef96
Omar Khair <omar.khair70@gmail.com>
2026-09-18 10:20:04 +0300

    spike15: add Spring Boot integration server entrypoint

---
+++ spikes/e2e-vertical-server/src/main/kotlin/com/hiltech/spike15/Spike15Server.kt
@@ -0,0 +1,11 @@
+package com.hiltech.spike15
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spike15Server
+
+fun main(args: Array<String>) {
+    runApplication<Spike15Server>(*args)
+}
