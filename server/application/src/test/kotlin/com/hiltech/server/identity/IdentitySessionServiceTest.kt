f46f6113436aaea70e6aaed56d340d3bef4f38c0
Omar Khair <omar.khair70@gmail.com>
2026-09-19 08:28:15 +0300

    test: use kotlin junit assertThrows

--- server/application/src/test/kotlin/com/hiltech/server/identity/IdentitySessionServiceTest.kt
+++ server/application/src/test/kotlin/com/hiltech/server/identity/IdentitySessionServiceTest.kt
@@ -1,7 +1,7 @@
 package com.hiltech.server.identity
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
